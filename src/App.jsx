1db854315b7b1ad1c198b635857c1cdb0551b027
jayrboy <j.jayox_@hotmail.com>
2024-01-01 01:47:38 +0700

    Display API
    Display API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import "./App.css";
+import Picture from "./components/Picture";
 
 function App() {
   const [word, setWord] = useState("");
   const key = "p1fkPloDSYpBNNzVeXGT1z6oBb88gZY4o2DVrMDWov0";
+  const [photos, setPhotos] = useState([]);
 
   function searchImage(e) {
     e.preventDefault();
@@ -19,9 +21,17 @@ function App() {
     const url = `https://api.unsplash.com/search/photos?page=1&query=${word}&client_id=${key}&per_page=15`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
-  }
+    const result = data.results;
 
+    if (result.length == 0) {
+      alert("ไม่มีรูปภาพ");
+      setWord("");
+    } else {
+      // แสดงข้อมูล
+      setPhotos(result);
+      console.log(result);
+    }
+  }
   return (
     <>
       <h1>ระบบค้นหารูปภาพด้วย API</h1>
@@ -34,6 +44,12 @@ function App() {
         />
         <button type="submit">ค้นหา</button>
       </form>
+      {/* แสดงข้อมูลรูปภาพ */}
+      <div className="search-result">
+        {photos.map((data, index) => {
+          return <Picture {...data} key={index} />;
+        })}
+      </div>
     </>
   );
 }
